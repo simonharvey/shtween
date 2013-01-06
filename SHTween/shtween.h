b37c0d7e2e65432303e666da5605695fc1307bf5
Simon Harvey <harveysimon@gmail.com>
2013-01-06 13:26:20 -0500

    added easing function and removed unused typedefs

--- SHTween/shtween.h
+++ SHTween/shtween.h
@@ -15,14 +15,54 @@
 using namespace std;
 
 namespace shtween
-{
-	typedef void (*tween_callback_fun)();
-	
+{	
 	namespace easing
 	{
+		using std::pow;
+		using std::cos;
+		using std::sin;
+	
+#define PI		3.141592654
+#define HALF_PI 1.570796327
+		
 		template <typename T>
 		inline T linear(float t, const T &b, const T &c, float d) {
 			return c*t/d + b;
+		}
+		
+		template <typename T>
+		inline T expo_in(float t, const T &b, const T &c, float d) {
+			float p = pow(2, 10 * (t/d - 1));
+			return (t==0) ? b : c * p + b;
+		}
+
+		template <typename T>
+		inline T expo_out(float t, const T &b, const T &c, float d) {
+			auto res = (t==d) ? b+c : c * (-(float)pow(2.0f, -10 * t/d) + 1) + b;
+			return res;
+		}
+		
+		template <typename T>
+		inline T expo_in_out(float t, const T &b, const T &c, float d) {
+			if (t == 0) return b;
+			if (t == d) return b+c;
+			if ((t/=d/2) < 1) return c/2.0f * (float)pow(2, 10*(t-1))+b;
+			return c/2.0f * (-(float)pow(2, -10 * --t)+2) + b;
+		}
+		
+		template <typename T>
+		inline T sine_in(float t, const T &b, const T &c, float d) {
+			return -c * cos(t/2.0f*HALF_PI) + c + b;
+		}
+		
+		template <typename T>
+		inline T sine_out(float t, const T &b, const T &c, float d) {
+			return c * sin(t/d*HALF_PI) + b;
+		}
+		
+		template <typename T>
+		inline T sine_in_out(float t, const T &b, const T &c, float d) {
+			return -c/2.0f * (float)(cos(PI * t/d) - 1) + b;
 		}
 	}
 	
@@ -38,11 +78,6 @@ namespace shtween
 		virtual bool complete() = 0;
 	};
 	
-	struct TweenCallbacks
-	{
-		tween_callback_fun on_complete;
-	};
-	
 	template <typename T>
 	struct ValueTween : Tween
 	{
@@ -56,7 +91,7 @@ namespace shtween
 		T &property;
 		T start_value, end_value;
 		
-		ValueTween(T &property, T value, float duration = 1.0f) : property(property), running(false), end_value(value), duration(duration), t(0), easing(easing::linear<T>) {
+		ValueTween(T &property, T value, float duration = 1.0f, easing_fun easing = easing::linear<T>) : property(property), running(false), end_value(value), duration(duration), t(0), easing(easing) {
 			
 		}
 		
@@ -171,9 +206,9 @@ namespace shtween
 		}
 		
 		template <typename T>
-		TweenGroupBuilder &to(T &property, T value, float duration = 1.0f)
+		TweenGroupBuilder &to(T &property, T value, float duration = 1.0f, typename ValueTween<T>::easing_fun easing = easing::linear)
 		{
-			result->children.push_back(new ValueTween<T>(property, value, duration));
+			result->children.push_back(new ValueTween<T>(property, value, duration, easing));
 			return *this;
 		}
 		
